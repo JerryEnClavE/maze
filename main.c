282c3e9e893ff6720a6d91c21368f9e06d0d379b
JerryEnClavE <jerrymeletiche123@gmail.com>
2025-02-28 16:39:45 -0400

    >>>

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "map.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
---
+++ map.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define MAP_WIDTH 10
+#define MAP_HEIGHT 10
+
+int map[MAP_WIDTH][MAP_HEIGHT] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
+
+void print_map() {
+    for (int y = 0; y < MAP_HEIGHT; y++) {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            printf("%d ", map[x][y]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    print_map();
+    return 0;
+}
