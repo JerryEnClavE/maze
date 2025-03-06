ad9a47b50c18396cd43165f1529d8e6959b5e05b
JerryEnClavE <jerrymeletiche123@gmail.com>
2025-03-06 14:26:49 -0400

    >>>

--- src/textures.c
+++ src/textures.c
@@ -1,7 +1,5 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
-#include "../inc/textures.h"
-#include "../inc/map.h"
 
 SDL_Texture* floor_texture = NULL;
 SDL_Texture* ceiling_texture = NULL;
@@ -13,7 +11,5 @@ SDL_Texture* load_texture(SDL_Renderer* renderer, const char* path) {
         printf("Failed to load texture: %s\n", IMG_GetError());
         return NULL;
     }
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
     return texture;
 }
