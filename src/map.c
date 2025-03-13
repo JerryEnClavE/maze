5fed2edc3f5aa3a660135877388d1cb182d4e5e2
JerryEnClavE <jerrymeletiche123@gmail.com>
2025-03-13 11:46:27 -0400

    >>>

--- src/map.c
+++ src/map.c
@@ -13,13 +13,3 @@ int map[MAP_HEIGHT][MAP_WIDTH] = {
     {1, 0, 1, 0, 1, 0, 0, 0, 0, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
-
-// Define the positions of the enemies in the map
-Enemy enemies[] = {
-    {128, 128, 0, NULL}, // Example positions
-    {256, 128, 0, NULL},
-    {384, 128, 0, NULL},
-    {512, 128, 0, NULL},
-    {640, 128, 0, NULL}
-};
-int num_enemies = sizeof(enemies) / sizeof(enemies[0]);
